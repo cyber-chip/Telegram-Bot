d8f47ff49cfb0bb4493182c0050c471de761c804
Farit Karimov <cyber-chip@yandex.ru>
2022-02-18 11:41:10 +0500

    Создание Бота для Телеграм

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+import settings
 #Будем записывать отчет о работе бота
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
+# Настройки прокси
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {
+        'username': settings.PROXY_USERNAME,
+        'password': settings.PROXY_PASSWORD
+    }
+}
 
 def main():
     # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
-    mybot = Updater("Key")
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     #будем использовать диспетчер mybot.dispatcher для того, 
     # чтобы при наступлении события вызывалась наша функция:
